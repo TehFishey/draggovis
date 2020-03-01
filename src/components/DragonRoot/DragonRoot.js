1c5ba69b687bfaff9a36e5c2a3992cc13c8a64e8
Michael D'Attilo <anuncertaintruth@gmail.com>
2020-03-01 14:40:45 -0500

    Lifted State to DragonRoot Component
    Altered component state/property management to store tree's state entirely in the tree root object; changes to fields are propogated up recursively generated tree and fed into a state change at root.

--- src/components/DragonRoot/DragonRoot.js
+++ src/components/DragonRoot/DragonRoot.js
@@ -15,32 +15,11 @@ export default class DragonRoot extends React.Component {
     return ( 
     <div className="lineage-element">
       <ul id ="tree-root">
-        <DragonElement data={this.state.data} />
+        <DragonElement 
+          data={this.state.data} 
+          onChange={(data) => this.setState({data})}
+          />
       </ul>
     </div>)
   }
 }
-
-/*
-
-
-
-export default class DragonRoot extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-    <div>
-      <ul className="dragon-root">
-        { DragonElement(props.dragon) }
-      </ul>
-    </div>
-    )
-  }
-}
-
-export default App;
-*/

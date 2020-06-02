1cd8d0882e949505a1532384e6e0fd320d92ceab
Michael D'Attilo <39676645+TehFishey@users.noreply.github.com>
2020-06-02 12:20:15 -0400

    Reversed male and female
    Swapped mother/father positions in linage trees.

--- src/components/treeview/TreeElement.js
+++ src/components/treeview/TreeElement.js
@@ -22,19 +22,19 @@ export default class DragonElement extends React.Component {
   render () {
     let parents =[];
 
-    if (this.props.data.mother !== undefined) {
-      parents.push(
-        <DragonElement 
-          data={this.props.data.mother} 
-          onChange={(parentData) => this.onIncomingFieldChange(this.props.data.mother, parentData)}
-        />);
-    }
     if (this.props.data.father !== undefined) {
       parents.push(
       <DragonElement 
         data={this.props.data.father} 
         onChange={(parentData) => this.onIncomingFieldChange(this.props.data.father, parentData)}
       />);
+    }
+    if (this.props.data.mother !== undefined) {
+      parents.push(
+        <DragonElement 
+          data={this.props.data.mother} 
+          onChange={(parentData) => this.onIncomingFieldChange(this.props.data.mother, parentData)}
+        />);
     }
 
     return ( 

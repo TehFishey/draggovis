85cc6f189185528cf71354d0ed743958927ee8dd
Michael D'Attilo <39676645+TehFishey@users.noreply.github.com>
2020-06-02 13:39:10 -0400

    Refactored component tree
    Resolves issue #1

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Stage from './components/Stage';
+import './app.css';
+
+function App() {
+    return (
+        <div className="app-root">
+            <div className="app-header"></div>
+            <Stage />
+            <div className="App-footer"></div>
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import React from 'react';
+import Stage from './components/Stage';
+import './app.css';
+
+function App() {
+    return (
+        <div className="app-root">
+            <div className="app-header"></div>
+            <Stage />
+            <div className="App-footer"></div>
+        </div>
+    );
+}
+
+export default App;
+*/
---
+++ src/components/Stage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import DragonElement from './treeview/TreeElement';
+import './treeview/tree-root.css';
+
+import testData from '../TestData';
+
+export default class Stage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: testData,
+        }
+    }
+
+    render () {
+        return ( 
+            <div className='app-stage'>
+                <div className='stage-top-menu'></div>
+                <div className='stage-side-menu'></div>
+                <div className='stage-canvas'>
+                    <div className="lineage-element">
+                        <ul id ="tree-root">
+                            <DragonElement 
+                            data={this.state.data} 
+                            onChange={(data) => this.setState({data})}
+                            />
+                        </ul>
+                    </div>
+                </div>    
+            </div>
+        );
+    }
+}
